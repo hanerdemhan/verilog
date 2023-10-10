8b6e1f40ba603e06382e4f009067e423f0528636
hanerdemhan <109851942+hanerdemhan@users.noreply.github.com>
2023-10-10 09:55:17 +0300

    Create shift_register.v

---
+++ shift_register.v
@@ -0,0 +1,21 @@
+// 8 bit shift register
+module sh_8_bit_hdl( 
+    input clk,
+    input [7:0] data,
+    input rst,
+    output reg [15:0] data_out
+    );
+    
+    always @(posedge clk or posedge rst)
+        begin
+            if (rst) 
+                begin
+                    data_out <= 16'b0000000010101010; // Initial value: 0000000010101010
+                end 
+            else 
+                begin
+                    data_out [7:0] <= data[7:0];
+                    data_out <= data_out << 8; // Shift left by 8 bits
+                end
+        end
+endmodule
